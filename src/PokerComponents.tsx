523b5ba310277de9c901cf3c46134cd21c02568c
eguneys <eguneys@gmail.com>
2024-07-19 16:56:30 +0000

    MiddleNProper

--- src/PokerComponents.tsx
+++ src/PokerComponents.tsx
@@ -1,4 +1,4 @@
-import { Card, Suit } from 'phevaluatorjs25'
+import { Card, Side, Suit } from 'phevaluatorjs25'
 import './Components.scss'
 import { Accessor, batch, createEffect, createMemo, createSignal, Index, JSX, Match, on, onCleanup, Show, Switch } from "solid-js"
 
@@ -74,13 +74,15 @@ export const CardView = (props: { class?: string, card: Card }) => {
     </>)
 }
 
-export const CardHolder = (props: { class?: string, card?: Card }) => {
+export const CardHolder = (props: { class?: string, elevate: Uk<boolean>, card: Uk<Card> }) => {
 
     return (<>
-      <div class='card-wrap'>
+      <div class={'card-wrap ' + (props.class ?? '')}>
         <div class='card-holder'></div>
-        <Show when={props.card}>{card => 
-          <CardView {...props} card={card()}></CardView>
+        <Show when={props.card.value}>{card => 
+          <div class={'elevate ' + props.elevate.klass}>
+            <CardView class={props.card.klass} card={card()}></CardView>
+          </div>
         }</Show>
       </div>
     </>)
@@ -89,37 +91,40 @@ export const CardHolder = (props: { class?: string, card?: Card }) => {
 
 export const Hand = (props: HandCards) => {
     return (<>
-    <div class='hand updatable updated'>
-        <CardHolder card={props.hand?.[0]} />
-        <CardHolder card={props.hand?.[1]} />
+    <div class={'hand'}>
+        <CardHolder {...props.hand[0]} />
+        <CardHolder {...props.hand[1]} />
     </div>
     </>)
 }
 
 type Uk<T> = { klass: string, value: T | undefined }
 
-type HandCards = { hand: Uk<[Card, Card]> }
-type MiddleCards = { flop: Uk<[Card, Card, Card]>, turn: Uk<Card>, river: Uk<Card> }
+
+type CardProp = { card: Uk<Card>, elevate: Uk<boolean>, back: Uk<boolean> } 
+& { 
+    _set_card: (_: Card | undefined) => void 
+    _set_elevate: (_: boolean | undefined) => void 
+    _set_back: (_: boolean | undefined) => void 
+}
+type HandCards = { hand: [CardProp, CardProp] }
+type MiddleCards = { flop: [CardProp, CardProp, CardProp], turn: CardProp, river: CardProp } & ShowdownInfo
+
+type ShowdownInfo = { showdown_info: Uk<string> }
 
 export const Middle = (props: MiddleCards) => {
     return (<>
     
     <div class='middle'>
-        <div class='showdown-info'>
-            <span>Ace High, 3 5</span>
+        <div class={'showdown-info ' + props.showdown_info.klass}>
+            <span>{props.showdown_info.value}</span>
         </div>
         <div class='ftr'>
-            <div class={'flop ' + props.flop.klass}>
-              <CardHolder class='flop-0' card={props.flop.value?.[0]}/>
-              <CardHolder class='flop-1' card={props.flop.value?.[1]}/>
-              <CardHolder class='flop-2' card={props.flop.value?.[2]}/>
-            </div>
-            <div class={'turn ' + props.turn.klass}>
-                <CardHolder card={props.turn.value} />
-            </div>
-            <div class={'river ' + props.river.klass}>
-                <CardHolder card={props.river.value} />
-            </div>
+            <CardHolder class={'flop ' + props.flop[0].card.klass} {...props.flop[0]}/>
+            <CardHolder class={'flop ' + props.flop[1].card.klass} {...props.flop[1]}/>
+            <CardHolder class={'flop ' + props.flop[2].card.klass} {...props.flop[2]}/>
+            <CardHolder class={'turn ' + props.turn.card.klass} {...props.turn} />
+            <CardHolder class={'river ' + props.river.card.klass} {...props.river} />
         </div>
     </div>
     </>)
@@ -141,38 +146,43 @@ type Bet = {
 }
 
 type PersonProps = HandCards & {
-    class?: string,
-    bet?: Bet,
-    chips: number,
-    state: string,
-    turn_left?: number
+    class: string,
+    bet: Uk<Bet>,
+    chips: Uk<number>,
+    state: Uk<string>,
+    turn_left: Uk<number>
+} & {
+    _set_chips: (_: number | undefined) => void
 }
 
 export const Person = (props: PersonProps) => {
 
-
-    let klass = ''
-    if (props.state === '@') { klass += ' turn' }
-    if (props.state === 'i') { klass += ' in' }
-    if (props.state === 'f') { klass += ' folded' }
-    if (props.class) { klass += ' ' + props.class }
+    const klass = createMemo(() => {
+        let state = props.state.value
+        let klass = ''
+        if (state === '@') { klass += ' turn' }
+        if (state === 'i') { klass += ' in' }
+        if (state === 'f') { klass += ' folded' }
+        klass += ' ' + props.class
+        return klass
+    })
 
     return (<>
-        <div class={'person ' + klass }>
+        <div class={'person ' + klass() }>
             <div class='handle'>
                 <span>Handle</span>
                 <Hand {...props} />
             </div>
             <div class='stack-wrap'>
-                <Show when={props.turn_left}>{ turn_left =>
+                <Show when={props.turn_left.value}>{ turn_left =>
                     <>
                     <div class='turn-left'><div class='bar' style={`width: ${turn_left()}%;`}></div></div>
                     </>
                 }</Show>
                 <div class='bets'>
-                    <Show when={props.bet}>{bet =>
+                    <Show when={props.bet.value}>{bet =>
                         <>
-                            <div class={'bet updatable updated ' + bet_klass(bet())}>
+                            <div class={['bet', props.bet.klass, bet_klass(bet())].join(' ')}>
                                 <Show when={bet().raise}>{raise =>
                                     <span class='raise'>Raise <InlineChips chips={raise()} /></span>
                                 }</Show>
@@ -192,7 +202,7 @@ export const Person = (props: PersonProps) => {
                         </>
                     }</Show>
                 </div>
-                <div class='stack updatable updated'> <h3>Stack</h3> <Chips chips={props.chips} /></div>
+                <div class={'stack ' + props.chips.klass}> <h3>Stack</h3> <Chips chips={props.chips.value ?? 0} /></div>
             </div>
         </div>
     </>)
@@ -218,7 +228,10 @@ export const MiddleNHands = (props: { people: PersonProps[], middle: MiddleCards
         </div>
         <div class='dealer'>
             <EventInfo/>
-            <CardHolder class=''/>
+            <div class='card-wrap'>
+                <div class='card-holder'></div>
+            </div>
+
             <Middle {...props.middle} />
             <div class={'pots pops ' + props.pot.klass}>
                 <h3>Main Pots</h3>
@@ -228,48 +241,136 @@ export const MiddleNHands = (props: { people: PersonProps[], middle: MiddleCards
             </div>
         </div>
         <div class='people'>
-            <Index each={props.people}>{(person, i) => 
-                <Person class={side_klass[i]} {...person()}/>
+            <Index each={props.people}>{(person) => 
+                <Person  {...person()}  />
             }</Index>
         </div>
     </>)
 }
 
+export const PersonProper = (i: Side) => {
+
+    const [_class, _set_class] = createSignal<string>(side_klass[i - 1])
+    const [_chips, _set_chips] = createSignal<number | undefined>(undefined)
+    const [_state, _set_state] = createSignal<string | undefined>(undefined)
+    const [_bet, _set_bet] = createSignal<Bet | undefined>(undefined)
+    const [_turn_left, _set_turn_left] = createSignal<number | undefined>(undefined)
+
+    let hand = UCardProper2({ init_delay: 300, update_delay: 2000, exit_delay: 300})
+    let chips = uklass(_chips, { init_delay: 100, update_delay: 200, exit_delay: 300})
+    let state = uklass(_state, { init_delay: 300, update_delay: 2000, exit_delay: 300})
+    let bet = uklass(_bet, { init_delay: 300, update_delay: 2000, exit_delay: 300})
+    let turn_left = uklass(_turn_left, { init_delay: 300, update_delay: 2000, exit_delay: 300})
+
+    return { _set_chips, turn_left, hand: hand.cards, chips, state, bet, get class() { return _class() } }
+}
+
+export const UCardProper = (opts: UOptions): CardProp => {
+    const [_card, _set_card] = createSignal<Card | undefined>(undefined)
+    const card = uklass(_card, opts)
+
+    const [_elevate, _set_elevate] = createSignal<boolean | undefined>(undefined)
+    const elevate = uklass(_elevate, { update_delay: 8000 })
+
+    const [_back, _set_back] = createSignal<boolean | undefined>(undefined)
+    const back = uklass(_back, opts)
+
+    return { 
+        card,
+        elevate,
+        back,
+        _set_card,
+        _set_elevate,
+        _set_back
+    }
+}
+
+export const UCardProper2 = (opts: UOptions) => {
+    return {
+    cards: [UCardProper(opts), UCardProper(opts)] as [CardProp, CardProp],
+    _set_cards(cards: [Card, Card] | undefined) {
+        this.cards[0]._set_card(cards?.[0])
+        this.cards[1]._set_card(cards?.[1])
+    }
+   }
+}
+
+export const UCardProper3 = (opts: UOptions) => {
+   return {
+    cards: [UCardProper(opts), UCardProper(opts), UCardProper(opts)] as [CardProp, CardProp, CardProp],
+    _set_cards(cards: [Card, Card, Card] | undefined) {
+        this.cards[0]._set_card(cards?.[0])
+        this.cards[1]._set_card(cards?.[1])
+        this.cards[2]._set_card(cards?.[2])
+    }
+   }
+}
+
+
+export const MiddleNProper = () => {
+    const [_pot, _set_pot] = createSignal<number | undefined>(undefined)
+    const [_people, _set_people] = createSignal<PersonProps[]>([PersonProper(1), PersonProper(2)])
+    const [_showdown_info, _set_showdown_info] = createSignal<string | undefined>(undefined)
+
+    let u_flop = UCardProper3({ init_delay: 300, update_delay: 2000, exit_delay: 300})
+    let u_turn = UCardProper({ init_delay: 2000, update_delay: 3000, exit_delay: 300})
+    let u_river = UCardProper({ init_delay: 4000, update_delay: 3000, exit_delay: 300})
+
+    let u_pot = uklass(_pot, { update_delay: 300, exit_delay: 1000 })
+    let u_showdown_info = uklass(_showdown_info, { update_delay: 6000, exit_delay: 1000 })
+
+
+    const middle = createMemo(() => ({ flop: u_flop.cards, turn: u_turn, river: u_river, showdown_info: u_showdown_info }))
+    const people = createMemo(() => _people())
+
+    return {
+        pot: u_pot,
+        get middle() {
+            return middle()
+        },
+        get people() {
+            return people()
+        },
+        set flop(cards: [Card, Card, Card] | undefined) {
+            u_flop._set_cards(cards)
+        },
+        set turn(card: Card | undefined) {
+            u_turn._set_card(card)
+        },
+        set river(card: Card | undefined) {
+            u_river._set_card(card)
+        },
+        set total_pot(pot: number | undefined) {
+            _set_pot(pot)
+        },
+    }
+}
+
+
+
 export const Showcase = () => {
 
-    const [_flop, _set_flop] = createSignal<[Card, Card, Card] | undefined>(undefined)
-    const [_turn, _set_turn] = createSignal<Card | undefined>(undefined)
-    const [_river, _set_river] = createSignal<Card | undefined>(undefined)
-    const [_pot, _set_pot] = createSignal<number | undefined>(undefined)
+    const u_m = MiddleNProper()
 
-    const flop: Accessor<[Card, Card, Card] | undefined> = () => _flop()
-    const turn = () => _turn()
-    const river = () => _river()
-    const pot = () => _pot()
-
-    let u_flop = uklass(flop, { init_delay: 300, update_delay: 2000, exit_delay: 300})
-    let u_turn = uklass(turn, { init_delay: 2000, update_delay: 3000, exit_delay: 300})
-    let u_river = uklass(river, { init_delay: 4000, update_delay: 3000, exit_delay: 300})
-
-    let u_pot = uklass(pot, { update_delay: 300, exit_delay: 1000 })
-
-    const middle = createMemo(() => ({ flop: u_flop, turn: u_turn, river: u_river }))
 
     setTimeout(() => {
-        _set_flop(['Ah', 'Ad', 'Ac'])
-        _set_turn('Td')
-        _set_river('Ts')
-        _set_pot(100)
+        u_m.flop = ['Ah', 'Ac', 'Ad']
+        u_m.turn = 'Td'
+        u_m.river = 'Ts'
+        u_m.total_pot = 100
+        u_m.people[0]._set_chips(1000)
+        //u_m._set_showdown_info('High Card A J 3')
+        //u_flop.cards[0]._set_elevate(true)
     }, 1000)
     setTimeout(() => {
 
-        _set_pot(2000)
+        u_m.total_pot = 2000
+        u_m.people[0]._set_chips(100)
     }, 5000)
     setTimeout(() => {
-        _set_flop(undefined)
-        _set_turn(undefined)
-        _set_river(undefined)
-        _set_pot(undefined)
+        u_m.turn = undefined
+        u_m.river = undefined
+        u_m.total_pot = undefined
     }, 10000)
 
     return (<>
@@ -277,7 +378,7 @@ export const Showcase = () => {
         <ShowcaseSection header='Card'>
             <>
             <div class='hands'>
-                <MiddleNHands pot={u_pot} people={[{turn_left: 20, chips: 1000, state: '@', bet: { chips: 100, raise: 20 }}, { state: '@', chips: 100 }]} middle={{...middle()}}/>
+                <MiddleNHands {...u_m}/>
             <div class='buttons'>
                 <button>Deal Cards</button>
                 <button>Deal Flop</button>
@@ -312,7 +413,7 @@ type UOptions = {
 
 const uklass = function<T>(target: Accessor<T | undefined>, opts: UOptions): Uk<T> {
 
-    let i_delay = opts.init_delay ?? 0
+    let i_delay = (opts.init_delay ?? 0) + 300
     let u_delay = i_delay + (opts.update_delay ?? 0)
     let e_delay = u_delay + (opts.exit_delay ?? 0)
 
@@ -331,14 +432,14 @@ const uklass = function<T>(target: Accessor<T | undefined>, opts: UOptions): Uk<
                 })
             }, e_delay)
         } else if (p !== t) {
-            batch(() => {
-              set_current(() => t)
-              set_klass('updatable init')
-            })
+            set_klass('updatable init')
 
             clear_id = setTimeout(() => {
-                set_klass('updatable updating')
-            }, 300 + i_delay)
+                batch(() => {
+                  set_klass('updatable updating')
+                  set_current(() => t)
+                })
+            }, i_delay)
 
             clear_id2 = setTimeout(() => {
                 set_klass('updatable updated')
